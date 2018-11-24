65d3a180f3063edae0992f1215027bc498f059c4
zhifeng ou <zhifengou@hotmail.com>
2018-11-24 20:57:54 +0000

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -11,8 +11,15 @@ class Pancake extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
+    componentDidMount() {
+      this.interval = setInterval(this.updateCounter, 1000)
+    }
 
   // TODO: create a componentWillUnmount() which will clear the interval
+
+      componentWillUnmount() {
+        clearInterval(this.interval)
+      }
 
   updateCounter = () => {
     this.setState({
@@ -20,13 +27,13 @@ class Pancake extends React.Component {
     });
   };
 
-  startInterval = () => {
-    this.interval = setInterval(this.updateCounter, 1000);
-  };
+  // startInterval = () => {
+  //   this.interval = setInterval(this.updateCounter, 1000);
+  // };
 
-  cleanUpInterval = () => {
-    clearInterval(this.interval);
-  };
+  // cleanUpInterval = () => {
+  //   clearInterval(this.interval);
+  // };
 
   flip = () => {
     this.setState({
